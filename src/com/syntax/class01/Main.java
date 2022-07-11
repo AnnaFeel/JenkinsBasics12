ba317d51dc85f9bde0e3ddb8875092b2c7f407b8
Anna Filippova <99766744+AnnaFeel@users.noreply.github.com>
2022-07-11 14:11:53 +1000

    second commit

--- src/com/syntax/class01/Main.java
+++ src/com/syntax/class01/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         for(int i=0; i<4; i++){
             System.out.println("Hello world");
+            System.out.println("Hello, I am a dog");
         }
     }
 }
